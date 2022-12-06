74449bab83fafbfc8580d8b1e6f8a2c1ef8e6b58
ram-221 <rameshmalli857.com>
2022-12-06 05:58:27 +0530

    Added: Analyse method use can return HAPPY

--- lib/src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
 
-/*8analyseMood method can just return
-		SAD to pass this Test Case (TC)*/
+/* analyseMood method can just return
+		Happy to pass this Test Case (TC)*/
     @Test
     public void givenMessage_WhenProper_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyserMood("I am in sad mood");
+        String mood = moodAnalyser.analyserMood("I am in any mood");
         System.out.println(mood);
-        Assertions.assertEquals("SAD", mood);
+        Assertions.assertEquals("HAPPY", mood);
     }
 }
