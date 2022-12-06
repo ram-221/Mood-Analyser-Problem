1158f79db461fc00d16c646a86787593a72e1dc4
ram-221 <rameshmalli857.com>
2022-12-06 06:31:40 +0530

    Added: sad mood in constructor should return SAD

--- lib/src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,22 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
 
-	/* MoodAnalyser will have 2 Constructors 1.Default - MoodAnalyser() and with
-              2.Parameters – MoodAnalyser(message)*/
 
-	@Test
-	public void givenMessage_WhenProper_ShouldReturnSad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
-		String sadMood = moodAnalyser.analyseMood();
-		System.out.println(sadMood);
-		Assertions.assertEquals("SAD", sadMood);
-	}
-
-	@Test
-	public void givenMessage_WhenProper_ShouldReturnHappy() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
-		String happyMood = moodAnalyser.analyseMood();
-		System.out.println(happyMood);
-		Assertions.assertEquals("HAPPY", happyMood);
-	}
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String sadMood = moodAnalyser.analyseMood();
+        System.out.println(sadMood);
+        Assertions.assertEquals("SAD", sadMood);
+    }
 }
