b8878447d34107441252a0148217368049875c4a
ram-221 <rameshmalli857.com>
2022-12-06 06:40:12 +0530

    Added: Happy Mood in constructor should return HAPPY

--- lib/src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -8,9 +8,9 @@ public class MoodAnalyserTest {
 
     @Test
     public void givenMessage_WhenProper_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
         String sadMood = moodAnalyser.analyseMood();
         System.out.println(sadMood);
-        Assertions.assertEquals("SAD", sadMood);
+        Assertions.assertEquals("HAPPY", sadMood);
     }
 }
